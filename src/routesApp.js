c108ecae52fa61d37d000036961061d670d342e8
Kleber Alves <kleberjuniorr63@gmail.com>
2022-08-26 17:13:42 -0300

    [Refs 0001 - FEAT - Implementado primeiros templates de teste e rotas]

---
+++ src/routesApp.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from './pages/index/Index'
+import Register from './pages/register/Register'
+
+function RoutesApp() {
+    return (
+        <Routes>
+            <Route path="/" element={<Index/>} />
+            <Route path="/register" element={<Register/>} />
+        </Routes>
+    );
+
+};
+
+
+export default RoutesApp;
